4654405dc339689990b6da2ea5ec3f05c508a778
Hare Sahani <harecareer@gmail.com>
2025-03-26 16:48:30 +0530

    update

--- src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
+++ src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
@@ -6,40 +6,23 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Document(collection = "journal_entries")
+@Data  // Lombok generates getters, setters, toString, equals, and hashCode
+@NoArgsConstructor  // Generates a no-args constructor
+@AllArgsConstructor // Generates an all-args constructor
 public class JournalEntry {
 
     @Id
     private ObjectId id;
 
     private String title;
-
     private String content;
-
     private LocalDateTime date;
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-    public ObjectId getId() {
-        return id;
-    }
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-    public String getTitle() {
-        return title;
-    }
-    public void setTitle(String title) {
-        this.title = title;
-    }
-    public String getContent() {
-        return content;
-    }
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
+
+
+//journalApp/src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
